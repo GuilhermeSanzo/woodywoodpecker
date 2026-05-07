d1052bdc7f333c2e708a8ef2dd1986abe527feae
Guilherme Souza <81893267+GuilhermeSanzo@users.noreply.github.com>
2026-05-07 12:35:30 -0300

    feat: register resource routes for all application entities in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\DistributorController;
+use App\Http\Controllers\PromotionController;
+use App\Http\Controllers\UserTypeController;
+use App\Http\Controllers\StoreController;
+use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +28,18 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('authors', AuthorController::class);
+Route::resource('publishers', PublisherController::class);
+Route::resource('genres', GenreController::class);
+Route::resource('distributors', DistributorController::class);
+Route::resource('promotions', PromotionController::class);
+Route::resource('user-types', UserTypeController::class);
+Route::resource('stores', StoreController::class);
+Route::resource('newsletters', NewsletterController::class);
+Route::resource('abouts', AboutController::class);
+Route::resource('contacts', ContactController::class);
+Route::resource('books', BookController::class);
+Route::resource('users', UserController::class);
 
 require __DIR__.'/auth.php';

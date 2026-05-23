d7a8c5ef88271b1999984dd3943329d6340647c1
Guilherme Souza <81893267+GuilhermeSanzo@users.noreply.github.com>
2026-05-22 23:12:53 -0300

    fix: resolve dynamic cart item counter in navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,14 +28,15 @@
                         </svg>
                         {{ __('My Orders') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('cart.index')" :active="request()->routeIs('cart.*')" class="relative">
+                    <x-nav-link :href="route('cart.index')" :active="request()->routeIs('cart.*')" class="flex items-center">
                         <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                         </svg>
                         {{ __('Cart') }}
-                        @if(count(session('cart', [])) > 0)
-                            <span class="absolute -top-1 -right-2 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-indigo-600 rounded-full">
-                                {{ count(session('cart', [])) }}
+                        @php $cartTotalQuantity = array_sum(array_column((array) session('cart', []), 'quantity')); @endphp
+                        @if($cartTotalQuantity > 0)
+                            <span class="bg-indigo-600 text-white text-xs font-bold px-2 py-0.5 rounded-full ml-2">
+                                {{ $cartTotalQuantity }}
                             </span>
                         @endif
                     </x-nav-link>
@@ -121,9 +122,10 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                 </svg>
                 {{ __('Cart') }}
-                @if(count(session('cart', [])) > 0)
+                @php $cartTotalQuantity = array_sum(array_column((array) session('cart', []), 'quantity')); @endphp
+                @if($cartTotalQuantity > 0)
                     <span class="ml-auto bg-indigo-600 text-white px-2 py-0.5 rounded-full text-xs font-bold">
-                        {{ count(session('cart', [])) }}
+                        {{ $cartTotalQuantity }}
                     </span>
                 @endif
             </x-responsive-nav-link>
